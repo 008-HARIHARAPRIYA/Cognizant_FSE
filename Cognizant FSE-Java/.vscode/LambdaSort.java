74e85064813182427357d0086580ee8252d9a695
008-HARIHARAPRIYA <121345379+008-HARIHARAPRIYA@users.noreply.github.com>
2025-05-26 11:21:03 +0530

    third commit

--- Cognizant FSE-Java/.vscode/LambdaSort.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class LambdaSort {
-    public static void main(String[] args) {
-        List<String> list = Arrays.asList("Banana", "Apple", "Cherry", "Date");
-        Collections.sort(list, (s1, s2) -> s1.compareToIgnoreCase(s2));
-        System.out.println("Sorted list: " + list);
-    }
-}
